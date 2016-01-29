ef591652a2a7a9b82c37a8f8f69bdf434f641a48
Brooke Hurford <brookebhurford@gmail.com>
2016-01-28 22:10:54 -0800

    Update specs to be cleaner, and have passing for creating pizza and topping objects, and passing for creating baseline price

--- js/master.js
+++ js/master.js
@@ -1,24 +1,21 @@
+function Toppings(name, addedPrice) {
+	this.name = name;
+	this.addedPrice = addedPrice;
+}
+
 function Pizza(pieSize, toppings, price) {
 	this.pieSize = pieSize;
 	this.toppings = [""];
 	this.price = price;
 };
 
-// Pizza.prototype.totalPrice = function() {
-// 	if (testPizza.toppings === ["pepperoni"]) {
-// 		return this.price + this.addedPrice;
-// 	}
-// }
+Pizza.prototype.basePrice = function() {
+	var price = 15;
 
-// this.toppings = this.totalPrice
-
-// Pizza.prototype.basePrice = function() {
-// 	var price = 15;
-//
-// 	if (this.pieSize === "large") {
-// 	 return price;
-//  	}
-// 	if (this.pieSize === "small") {
-// 	 return price -= 5;
-// 	}
-// }
+	if (this.pieSize === "large") {
+	 return price;
+ 	}
+	if (this.pieSize === "small") {
+	 return price -= 5;
+	}
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,21 +1,21 @@
 describe ('PizzaOrder', function() {
-  it("creates a new pizza with the given properties", function() {
+  it("creates a new pizza order with the given properties", function() {
     var testPizza = new Pizza("large", [""], 15);
     expect(testPizza.pieSize).to.equal("large");
-    expect(testPizza.toppings).to.equal["pepperoni", "sausauge", "olives", "green peppers"];
+    expect(testPizza.toppings).to.equal[""];
     expect(testPizza.price).to.equal(15);
   });
-
-  it('creates baseline price for pizza depending on its size', function() {
+  //
+  it("adds the baseline price method to the order", function() {
     var testPizza = new Pizza("large", [""], 15);
-    expect(testPizza.pieSize).to.equal(15);
+    expect(testPizza.basePrice()).to.equal(15);
   });
 });
 
-// describe ('Toppings', function() {
-//   it('creates a new topping with the given properties', function() {
-//     var testToppings = new Toppings("pepperoni", 1);
-//     expect(testToppings.name).to.equal("pepperoni");
-//     expect(testToppings.addedPrice).to.equal(1);
-//   })
-// })
+describe ('Toppings', function() {
+  it('creates a new topping with the given properties', function() {
+    var testToppings = new Toppings("pepperoni", 1);
+    expect(testToppings.name).to.equal("pepperoni");
+    expect(testToppings.addedPrice).to.equal(1);
+  })
+})
